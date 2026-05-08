574686aae968aed713763b023bd07a5f974d3bc4
IGdotnet <belonfranzolinigor@gmail.com>
2026-05-08 19:27:01 -0300

    LettersUsed

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Tip } from "./components/Tip"
 import { Letter } from "./components/Letter"
 import { Input } from "./components/Input"
 import { Button } from "./components/Button"
+import { LettersUsed } from "./components/LettersUsed"
 
 export default function App() {
 
@@ -27,6 +28,9 @@ export default function App() {
           <Input autoFocus maxLength={1} placeholder="?"/>
           <Button title="Confirmar"/>
         </div>
+
+        <LettersUsed />
+
       </main>
     </div>
   )
