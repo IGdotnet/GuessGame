9a1baa5919039f51375c4f0fa7f8c9375759459b
IGdotnet <belonfranzolinigor@gmail.com>
2026-05-07 21:50:11 -0300

    Letter

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./app.module.css"
 
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
 
 export default function App() {
 
@@ -14,6 +15,8 @@ export default function App() {
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame}/>
         <Tip tip="Linguagem de programação dinâmica"/>
+
+        <Letter value="R"/>
       </main>
     </div>
   )
