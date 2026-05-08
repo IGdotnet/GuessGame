796b827530c59022ebf5b9b594a5d3f246213d76
IGdotnet <belonfranzolinigor@gmail.com>
2026-05-08 19:11:17 -0300

    Input component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styles from "./app.module.css"
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
 import { Letter } from "./components/Letter"
+import { Input } from "./components/Input"
 
 export default function App() {
 
@@ -18,6 +19,11 @@ export default function App() {
 
         <div className={styles.word}>
           <Letter value="R"/>
+        </div>
+
+        <h4>Palpite</h4>
+        <div>
+          <Input autoFocus maxLength={1} placeholder="?"/>
         </div>
       </main>
     </div>
